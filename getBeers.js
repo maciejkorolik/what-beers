df00f4d03df6d11fd93f7d2a1196f15e25ddd24a
Maciej Korolik <46086480+maciejkorolik@users.noreply.github.com>
2022-07-15 11:22:28 +0200

    Parse html without puppeteer (#2)
    * parse html without puppeteer
    
    * remove puppeteer from package.json

--- getBeers.js
+++ getBeers.js
@@ -1,36 +1,34 @@
-const puppeteer = require("puppeteer");
+import fetch from "node-fetch";
+import { parse } from "node-html-parser";
 
 async function getBeers(url) {
-  const browser = await puppeteer.launch({
-    args: ["--lang=pl-PL,pl"],
+  const response = await fetch(url);
+  const html = await response.text();
+  const parsedHTML = parse(html);
+
+  // Query divs with beers and remove last element as it's not a real beer :(
+  const wrappers = Array.from(
+    parsedHTML.querySelectorAll(".panel.panel-default")
+  ).slice(0, -1);
+
+  const beers = wrappers.map((el) => {
+    const nameElement = el.querySelector("h4.cml_shadow > span");
+    const beerType = el.querySelector("span.cml_shadow > b").textContent.trim();
+    const breweryName = nameElement
+      .querySelector("b.brewery")
+      .text.replace(/\n|\r|\t/g, "")
+      .trim();
+    const brs = nameElement.querySelectorAll("br");
+    const beerName = brs[0].nextSibling.text.replace(/\n|\r|\t/g, "").trim();
+    const beerData = brs[1].nextSibling.text.replace(/\n|\r|\t/g, "").trim();
+    return {
+      brewery: breweryName,
+      name: beerName,
+      data: beerData,
+      type: beerType,
+    };
   });
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  const beers = await page.evaluate(() => {
-    // Query divs with beers and remove last element as it's not a real beer :(
-    const wrappers = Array.from(
-      document.querySelectorAll(".panel.panel-default")
-    ).slice(0, -1);
-
-    const descriptions = wrappers.map((el) => {
-      const nameElement = el.querySelector("h4.cml_shadow > span");
-      const type = el.querySelector("span.cml_shadow > b").textContent;
-      const nameParts = nameElement.innerText
-        .split("\n")
-        .map((el) => el.trim());
-      return {
-        brewery: nameParts[0],
-        name: nameParts[1],
-        data: nameParts[2],
-        type,
-      };
-    });
-
-    return descriptions;
-  });
-  await browser.close();
   return beers;
 }
 
-module.exports = getBeers;
+export default getBeers;
