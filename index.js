2c038fc845621d7c882880b56d60402821906ca2
Maciej Korolik <maciej.korolik@monterail.com>
2022-07-15 11:33:28 +0200

    try slack webhook

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import getBeers from "./getBeers.js";
 
 async function init() {
-  const beers = await getBeers("https://4hops.ontap.pl");
-  console.log(beers);
+  const beers = await getBeers("https://ontap.pl/?multitap_id=298");
+  await fetch(
+    "https://hooks.slack.com/services/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",
+    {
+      method: "post",
+      body: JSON.stringify({ text: "test 🍺" }),
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 }
 
 init();
---
+++ prepareSlackMessage.js
@@ -0,0 +1,3 @@
+function prepareSlackMessage(beers) {}
+
+export default prepareSlackMessage;
