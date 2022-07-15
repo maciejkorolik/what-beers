1dac3b5f82618431f1fddae5f71c01a03ddc1897
Maciej Korolik <maciej.korolik@monterail.com>
2022-07-15 10:18:05 +0200

    rearrange files

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
-const puppeteer = require("puppeteer");
+const getBeers = require("./getBeers");
 
 async function init() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://4hops.ontap.pl/");
-
-  const beers = await page.evaluate(() => {
-    // Query divs with beers and remove last element as it's not a real beer :(
-    const wrappers = Array.from(
-      document.querySelectorAll(".panel.panel-default")
-    ).slice(0, -1);
-
-    const descriptions = wrappers.map((el) => {
-      const nameElement = el.querySelector("h4.cml_shadow > span");
-      return nameElement.innerText.split("\n").map((el) => el.trim());
-    });
-
-    const structuredDescriptions = descriptions.map((beer) => {
-      return {
-        brewery: beer[0],
-        type: beer[1],
-        data: beer[2],
-      };
-    });
-    return structuredDescriptions;
-  });
+  const beers = await getBeers("https://ontap.pl/?multitap_id=298");
   console.log(beers);
-  await browser.close();
 }
 
 init();
